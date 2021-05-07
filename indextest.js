bb188bfe1fdf224a04dc3e2edd8801d712948a28
Ivar Olsson <ivar.olsson2323@gmail.com>
2021-05-07 09:45:30 +0200

    typescript

---
+++ indextest.js
@@ -0,0 +1,5 @@
+var fruits = 'banana';
+function fullname(firstname, lastname) {
+    return firstname + " " + lastname;
+}
+console.log(fullname('ivar', 'Olsson'));
---
+++ indextest.ts
@@ -0,0 +1,8 @@
+let fruits: string = 'banana'; 
+
+
+function fullname (firstname: string, lastname:string): string {
+    return `${firstname} ${lastname}`
+}
+
+console.log(fullname('ivar', 'Olsson'))
